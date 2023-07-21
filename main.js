ff1a8f17dc93a4068b36d931bdcfe9ceb9b2f681
alif <xaliip16@gmail.com>
2023-07-21 11:08:31 +0700

    Belajar 2

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-let i = 11;
-
-switch (i) {
-    case 10:
-        console.log('Nilai Kamu Adalah ' + i)
-        break;
-    default:
-        console.log('Nilai Kamu Diluar Variable')
-        break;
-}
+ 
